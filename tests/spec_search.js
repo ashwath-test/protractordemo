07b195406362570a1fabe3acf4c7bbb5f65ae4fd
ashwath-test <39530814+ashwath-test@users.noreply.github.com>
2020-10-14 09:57:31 +0530

    added inline comment

--- tests/spec_search.js
+++ tests/spec_search.js
@@ -4,12 +4,25 @@ describe('Search Products', function () {
 
     var homepage = new HomePage();
 
-     it('search and validate', async () => {
+    it('search and validate', async () => {
+        // step 1: Load the page
         await browser.get('http://automationpractice.com/index.php')
 
+        // step 2: Find the Element and enter search text
+        // element(by.css("input[class='search_query form-control ac_input']")).sendKeys('Printed')
+
+        // Step 2 after converting to page object model ref:pages/homepage.js
         await homepage.enterSearchText('Printed');
+           //wait for 2 sec
         await browser.sleep(2000);
+
+        // step 3: Find the element and click search button
+        //  element(by.css("button[class='btn btn-default button-search']")).click();
+
+        // step 3
         await homepage.selectSearchOption();
+
+        //wait for 5 sec
         await browser.sleep(5000);
     })
 
