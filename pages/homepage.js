bf0f3ced3d49f3102c5809fd1135141bea930d85
ashwath-test <39530814+ashwath-test@users.noreply.github.com>
2020-10-14 13:38:33 +0530

    Added asssetions

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,9 +1,12 @@
+const { element } = require("protractor");
 
 var HomePage = function () {
 
     // locators
     this.searchField = element(by.css("input[class='search_query form-control ac_input']"));
     this.searchOption = element(by.css("button[class='btn btn-default button-search']"));
+    this.logoEl = element(by.css("div#header_logo"));
+    this.searchResultMsg = element(by.css("span.heading-counter"));
 
     // Functions
     this.enterSearchText = async (searchtext) => {
@@ -12,6 +15,11 @@ var HomePage = function () {
 
     this.selectSearchOption = async () => {
         await this.searchOption.click();
+    };
+
+    this.searchResultCount = async () => {
+       let text = await this.searchResultMsg.getText();
+       return parseInt(text.substring(0,1));
     };
 
 }
--- tests/spec_search.js
+++ tests/spec_search.js
@@ -1,20 +1,30 @@
 
-var HomePage = require('../pages/homepage.js')
+const { browser } = require('protractor');
+var HomePage = require('../pages/homepage.js');
+var EC = protractor.ExpectedConditions;
+
 describe('Search Products', function () {
 
     var homepage = new HomePage();
 
     it('search and validate', async () => {
         // step 1: Load the page
-        await browser.get('http://automationpractice.com/index.php')
+        await browser.get('http://automationpractice.com/index.php');
+
+        //wait page to load / logo to appear
+        await browser.wait(EC.visibilityOf(homepage.logoEl), 10000);
+
+        //Validate Title
+        expect(browser.getTitle()).toEqual('My Store');
 
         // step 2: Find the Element and enter search text
         // element(by.css("input[class='search_query form-control ac_input']")).sendKeys('Printed')
 
         // Step 2 after converting to page object model ref:pages/homepage.js
         await homepage.enterSearchText('Printed');
-           //wait for 2 sec
-        await browser.sleep(2000);
+        //wait for 2 sec
+        // await browser.sleep(2000);
+
 
         // step 3: Find the element and click search button
         //  element(by.css("button[class='btn btn-default button-search']")).click();
@@ -23,7 +33,14 @@ describe('Search Products', function () {
         await homepage.selectSearchOption();
 
         //wait for 5 sec
-        await browser.sleep(5000);
+        // await browser.sleep(5000);
+        // wait for result message to appear 
+        await browser.wait(EC.visibilityOf(homepage.searchResultMsg), 10000);
+
+        if (await homepage.searchResultCount() > 0) {
+
+            console.log("Results found")
+        }
     })
 
 })
